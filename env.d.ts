c1790939c5f93583a5ab95adfe833e059be395f2
Simon Monk-Chipman <simonm-c@hotmail.co.uk>
2025-06-06 23:46:36 +0100

    feat(icons): added unplugin-icons

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+// fix for cases where 'import xxx from ~icons/...' syntax is required
+declare module '~icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue'
+  const component: FunctionalComponent<SVGAttributes>
+  export default component
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), Icons({ compiler: 'vue3' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
